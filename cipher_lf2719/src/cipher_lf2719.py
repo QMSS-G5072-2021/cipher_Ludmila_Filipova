17724d065a91809d7dc8ce7133648d9da97812c1
Ludmila Filipova <filipova.lud@gmail.com>
2021-11-10 22:12:21 -0500

    Documentation

--- cipher_lf2719/src/cipher_lf2719.py
+++ cipher_lf2719/src/cipher_lf2719.py
@@ -1,4 +1,31 @@
+import pandas as pd
+import numpy as np
+
 def cipher(text, shift, encrypt=True):
+    """
+        Takes user's text input and encrypts it by shifting the letters by defined shift.
+
+        Parameters
+        ----------
+        text: pandas.Series.str
+            TEXT pandas character string.
+        shift: numpy.int64
+            SHIFT numpy integer that defines by how much will the letter shift from their position in the alphabet.
+
+        Returns
+        -------
+        pandas.Series.str
+            Encrypted string is pandas string.
+
+        Examples
+        --------
+        >>> from cipher_lf2719 import cipher_lf2719
+        >>> text = "hello world"
+        >>> shift = 5
+        >>> cipher("hello world", 5)
+        
+        'mjqqt Btwqi'
+    """
     alphabet = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ'
     new_text = ''
     for c in text:
