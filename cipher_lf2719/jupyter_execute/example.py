96fcbade97b38dc0a63676b2d8ed3b25c19d32de
Ludmila Filipova <filipova.lud@gmail.com>
2021-11-11 00:03:00 -0500

    added test

---
+++ cipher_lf2719/jupyter_execute/example.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# # Example usage
+# 
+# To use `cipher_lf2719` in a project:
+
+# In[1]:
+
+
+import cipher_lf2719
+
+print(cipher_lf2719.__version__)
+
--- cipher_lf2719/tests/test_cipher_lf2719.py
+++ cipher_lf2719/tests/test_cipher_lf2719.py
@@ -1,1 +1,8 @@
+import pytest
 from cipher_lf2719 import cipher_lf2719
+
+def test_single_word():
+    example = "hello"
+    expected = "ifmmp"
+    actual = cipher(example, 1)
+    assert actual == expected
